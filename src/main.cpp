c0a607b122ed33536b501dd535428eaaa8eb4341
Kris Chen <Krischen2003@gmail.com>
2025-04-15 16:58:24 -0400

    fixed cmakelist to match new folders and files, same for main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include "widgets/mainwindow.h"
+#include "widgets/infowindow.h"
+#include "settings/globalHotkeyfilter.h"
 #include <QApplication>
 #include <QSystemTrayIcon>
 #include <QMenu>
@@ -7,38 +8,43 @@
 #include <QObject>
 
 
+// TODO: for the future in localization of app
+void configureApp() {
 
-int main(int argc, char* argv[]) {
-    QApplication app(argc, argv);
+}
+// TODO: potentially separate tray icon step into its own widget
+void setupSystemTray() {
+    InfoWindow infoWindow;
 
-    QIcon appIcon("://icon.png");
-    app.setWindowIcon(appIcon);
-
-
-    MainWindow w;
-    MainWindow mainWindow;
-    //System Tray things
-    QSystemTrayIcon* trayIcon = new QSystemTrayIcon(QIcon("://icon.png"));
-    trayIcon->setToolTip("Ofnir");
+    QIcon appIcon(":/icon.png");
+    QSystemTrayIcon* trayIcon = new QSystemTrayIcon(appIcon);
 
     QMenu* menu = new QMenu();
     QAction* settingAction = new QAction("Setting", menu);
     QAction* quitAction = new QAction("Exit", menu);
 
-    //Buttons on system tray
+    // connects Setting action
     QObject::connect(settingAction, &QAction::triggered, [&]() {
-        mainWindow.show();
-        mainWindow.raise();
-        mainWindow.activateWindow();
+        infoWindow.show();
+        infoWindow.raise();
+        infoWindow.activateWindow();
         });
-
+    // connects Exit action
     QObject::connect(quitAction, &QAction::triggered, &app, &QApplication::quit);
-
+    // adds action to traymenu
     menu->addAction(settingAction);
     menu->addAction(quitAction);
-
+    // makes tray icon in system tray
+    trayIcon->setToolTip("Ofnir");
     trayIcon->setContextMenu(menu);
     trayIcon->show();
+}
+
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+
+
+    setupSystemTray();
 
     return app.exec();
 }
