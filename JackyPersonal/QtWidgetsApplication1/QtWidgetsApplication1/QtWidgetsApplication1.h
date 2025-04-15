f415446aa44d16aec9f0e999396e61fc49954946
Wskywonder <jackywong816@gmail.com>
2025-04-15 12:04:27 -0400

    simplified files, added setting function for colors, font, etc

--- JackyPersonal/QtWidgetsApplication1/QtWidgetsApplication1/QtWidgetsApplication1.h
+++ JackyPersonal/QtWidgetsApplication1/QtWidgetsApplication1/QtWidgetsApplication1.h
@@ -1,7 +1,9 @@
+#pragma once
+
 #include <QMainWindow>
 #include <QLabel>
-#include <QTextEdit>
 #include <QShortcut>
+#include <QKeySequence>
 #include "ui_QtWidgetsApplication1.h"
 
 #define HOTKEY_ID 1001
@@ -15,17 +17,17 @@ class QtWidgetsApplication1 : public QMainWindow
     Q_OBJECT
 
 public:
-    QtWidgetsApplication1(QWidget* parent = nullptr);
+    explicit QtWidgetsApplication1(QWidget* parent = nullptr);
     ~QtWidgetsApplication1();
 
 private slots:
     void captureAndShowScreenshot();
+
 private:
     QWidget* createResultPopup(const QString& text, const QRect& anchorRect);
+    void applyUserHotkey();  
 
 private:
     Ui::QtWidgetsApplication1Class ui;
-    QLabel* screenshotLabel;
-    QTextEdit* textOutput;
-    QShortcut* screenshotShortcut;
+    QShortcut* screenshotShortcut = nullptr;
 };
